daa281bff269d4a64a21be2760de8eb64a830058
CRACK37 <tech0support@crack37.win>
2025-12-15 21:49:33 +0000

    YmFzaWMgc2V0dXBf

---
+++ Plugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace ClosingBattle;
+
+[BepInPlugin(GUID, NAME, VERS)]
+public class Plugin : BaseUnityPlugin
+{
+    public const string GUID = "Q2xvc2luZ0JhdHRsZV9f";
+    public const string NAME = "ClosingBattle";
+    public const string VERS = "1.0.0";
+    
+    internal new static ManualLogSource Logger { get; private set; } = null!;
+    
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {GUID} is loaded!");
+        gameObject.hideFlags = HideFlags.DontSaveInEditor;
+    }
+}
