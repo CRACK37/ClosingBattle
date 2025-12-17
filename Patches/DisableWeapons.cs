5db8ac93ab8db992d76a2dbe4ba6797bcbe31ce6
CRACK37 <249635661+CRACK37@users.noreply.github.com>
2025-12-17 12:51:32 -0300

    QWRkIHNvbWUgbGlicmFyaWVzIGFuZCBhIHF1aWNrIG5vIHdlYXBvbnMgcGF0Y2g=

---
+++ Patches/DisableWeapons.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+
+namespace ClosingBattle.Patches;
+
+[HarmonyPatch]
+public static class DisableWeapons
+{
+    [HarmonyPatch(typeof(GunSetter), "Start"), HarmonyPrefix]
+    private static void GunSetterStart(
+        ref GunSetter __instance)
+    {
+        //Reset all weapons so we can never equip any of them
+        __instance.revolverPierce = [];
+        __instance.revolverRicochet = [];
+        __instance.revolverTwirl = [];
+
+        __instance.shotgunGrenade = [];
+        __instance.shotgunPump = [];
+        __instance.shotgunRed = [];
+
+        __instance.nailMagnet = [];
+        __instance.nailOverheat = [];
+        __instance.nailRed = [];
+
+        __instance.railCannon = [];
+        __instance.railHarpoon = [];
+        __instance.railMalicious = [];
+
+        __instance.rocketBlue = [];
+        __instance.rocketGreen = [];
+        __instance.rocketRed = [];
+    }
+    
+    
+}
--- Plugin.cs
+++ Plugin.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
+using System;
 using UnityEngine;
+
+using ClosingBattle.Patches;
 
 namespace ClosingBattle;
 
@@ -10,12 +13,19 @@ public class Plugin : BaseUnityPlugin
 {
     public const string GUID = "Q2xvc2luZ0JhdHRsZV9f";
     public const string NAME = "ClosingBattle";
-    public const string VERS = "1.0.0";
+    public const string VERS = "1.0.1";
     
     internal new static ManualLogSource Logger { get; private set; } = null!;
-    
+
+    public static Harmony harmony;
+
     private void Awake()
     {
+        if (harmony == null)
+            harmony = new Harmony(GUID);
+
+        harmony.PatchAll();
+
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {GUID} is loaded!");
         gameObject.hideFlags = HideFlags.DontSaveInEditor;
